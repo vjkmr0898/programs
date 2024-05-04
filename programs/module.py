6a379f240b2bd25e266b19abf54a1d3bf4e1f6e8
vjkmr0898 <arunvj5757@gmail.com>
2024-05-04 18:36:26 +0530

    checking attribute in module1 in module.py

--- programs/module.py
+++ programs/module.py
@@ -4,6 +4,7 @@ def user_input():
     name_to_add=input('Enter name to add: ')
     final_list=module1.converting_input(enter_name,name_to_add) #calling converting_input function from module1 
     print(final_list)
+    print(dir(module1))
     count_of_name=input('Enter the name to get a count: ')
     countOf_name=module1.count_of_element(final_list,count_of_name)  #calling count_of_element function from module1
     print(f'The count of {count_of_name} is {countOf_name}')
